30c5b616422a4580779a15683fdf394fedf3ef46
清水俊樹 <tamreff3290@gmail.com>
2021-07-18 13:32:29 +0900

    ディレクトリ構成を変更。python3 -m pytestが通ることを確認

---
+++ ExampleApp/tests/test_api.py
@@ -0,0 +1,7 @@
+import pytest
+from app.api import app
+def test_flask_simple():
+    app.config['TESTING'] = True
+    client = app.test_client() 
+    result = client.get('/')
+    assert b'Hello' in result.data
