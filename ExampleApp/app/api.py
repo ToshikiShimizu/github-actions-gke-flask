8dfeea08720f29871c2301fa32b3e18cbb5ae41e
清水俊樹 <tamreff3290@gmail.com>
2021-07-23 17:36:40 +0900

    autopep8を適用

--- ExampleApp/app/api.py
+++ ExampleApp/app/api.py
@@ -8,5 +8,6 @@ def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
